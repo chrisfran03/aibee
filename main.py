b376891626d30124879d9ae637c78bc511524f03
chrisfran03 <270155795@yoobeestudent.ac.nz>
2024-05-15 04:13:53 +1200

    .

--- main.py
+++ main.py
@@ -2,30 +2,33 @@ from openai import OpenAI
 import streamlit as st
 import time
 from dotenv import load_dotenv
+import os
 
-# client = OpenAI()
+load_dotenv()
 
-# model = "gpt-3.5-turbo"
+client = OpenAI()
+
+model = "gpt-3.5-turbo"
 
 
-# def get_response(question):
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=[{"role": "user", "content": question}]
-#     )
-#     return response.choices[0].message.content
+def get_response(question):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": question}]
+    )
+    return response.choices[0].message.content
 
 
 def main():
     st.title('Project Helper')
     user_input = st.text_input("Ask a question:")
 
-    # if st.button('Send'):
-    #     if user_input:
-    #         response = get_response(user_input)
-    #         st.text_area("Response:", value=response, height=300)
-    #     else:
-    #         st.error("Please enter a question.")
+    if st.button('Send'):
+        if user_input:
+            response = get_response(user_input)
+            st.text_area("Response:", value=response, height=300)
+        else:
+            st.error("Please enter a question.")
 
 
 if __name__ == '__main__':
